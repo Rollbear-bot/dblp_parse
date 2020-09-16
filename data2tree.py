825f3215fca527d62b12c42920703ebf93728e67
Junhong Wu <rollbearwu@qq.com>
2020-09-16 08:03:21 +0000

    [FEAT] the original dataset(while size) raise the integer overflow, use a mini dataset.

--- data2tree.py
+++ data2tree.py
@@ -15,24 +15,28 @@ import xml.etree.cElementTree as ET
 
 def main():
     # 待解析的xml文件的存储路径
-    xml_path = "/kaggle/input/dblp-raw/dblp-2020-09-01.xml"
+    xml_path = "../dblp-2020-09-01.xml"
     tmp_path = "./tmp.xml"
 
-    with open(xml_path, "r", encoding="utf8") as rf:
+    with open(xml_path, "r") as rf:
         xml_str = rf.read()
-        tmp = rf.readlines()[:12]
-        with open(tmp_path, "w", encoding="utf8") as wf:
-            wf.writelines(tmp)
+        # 构造小数据集
+        tmp = xml_str[:1000-167-14] + "</dblp>"
+
+        with open(tmp_path, "w") as wf:
+            wf.write(tmp)
 
     print("len of whole string", len(xml_str))
+    print("len of mini dataset", len(tmp))
 
     # 直接从字符串解析
-    root = ET.fromstring(xml_str)
+    root = ET.fromstring(tmp)
 
     # 查看解析内容
     for child in root:
         print(child.tag, child.attrib)
-
+        for c in child:
+            print(c.tag, c.attrib)
 
 if __name__ == '__main__':
     main()
